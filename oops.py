cc06819dc3f77c832234d0c84e169bb3d7416d8f
sreeja <sreeja.mudili@gmail.com>
2024-10-15 10:22:02 +0530

    Creating a class and object

---
+++ oops.py
@@ -0,0 +1,18 @@
+#######creating a class
+class Student:
+    name = "Anshu"
+##creating an object
+s1 = Student()
+print(s1.name)
+###################################
+class Car:
+    color = "Blue"
+    brand = "mercedes"
+car1 = Car()
+print(car1.color)
+print(car1.brand)
+
+
+
+
+
