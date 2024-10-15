888ef4bfad731f0a114e7a036fa9b5ee59aa0d55
sreeja <sreeja.mudili@gmail.com>
2024-10-15 15:49:13 +0530

    single inheritance

---
+++ multi level inheritance.py
@@ -0,0 +1,24 @@
+class car:
+    @staticmethod
+    def start():
+        print("car is started")
+    @staticmethod
+    def stop():
+        print("car is stopped")
+
+class Toyotocar(car):
+       def def__init__(self, type):
+           self.type = type
+class Fortunecar(Toyotocar):
+       def __init__(self, color):
+        self.color = color
+
+
+#car1 = Fortunecar("electric")
+car1 = Fortunecar("blue")
+print(car1.color)
+#print(car1.type)
+car1.start()
+car1.stop()
+
+
