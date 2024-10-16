83e8403314652d291734d34e7bb5fde0ba7bcc2e
sreeja <sreeja.mudili@gmail.com>
2024-10-16 10:50:03 +0530

    super method usage

--- multi level inheritance.py
+++ multi level inheritance.py
@@ -7,17 +7,17 @@ class car:
         print("car is stopped")
 
 class Toyotocar(car):
-       def def__init__(self, type):
+       def __init__(self, type):
            self.type = type
 class Fortunecar(Toyotocar):
        def __init__(self, color):
         self.color = color
 
 
-#car1 = Fortunecar("electric")
+car1 = Fortunecar("electric")
 car1 = Fortunecar("blue")
 print(car1.color)
-#print(car1.type)
+print(car1.type)
 car1.start()
 car1.stop()
 
